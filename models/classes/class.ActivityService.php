5ae11213d761f69cb919bff8be866d160572ca1f
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-03-19 14:14:02 +0000

    update license header
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/wfAuthoring/trunk@14105 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/class.ActivityService.php
+++ models/classes/class.ActivityService.php
@@ -1,43 +1,32 @@
 <?php
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2013 (original work) Open Assessment Techonologies SA (under the project TAO-PRODUCT);
+ *
+ *
+ */
 
 error_reporting(E_ALL);
 
 /**
  * TAO - wfAuthoring/models/classes/class.ActivityService.php
  *
- * $Id$
- *
- * This file is part of TAO.
- *
- * Automatically generated on 30.10.2012, 18:38:20 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- * @package wfAuthoring
- * @subpackage models_classes
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/**
  * Service that retrieve information about Activty definition during runtime
  *
- * @author Joel Bout, <joel.bout@tudor.lu>
- */
-require_once('wfEngine/models/classes/class.ActivityService.php');
-
-/* user defined includes */
-// section 10-30-1--78--3f16755c:13a9722f969:-8000:0000000000003BBF-includes begin
-// section 10-30-1--78--3f16755c:13a9722f969:-8000:0000000000003BBF-includes end
-
-/* user defined constants */
-// section 10-30-1--78--3f16755c:13a9722f969:-8000:0000000000003BBF-constants begin
-// section 10-30-1--78--3f16755c:13a9722f969:-8000:0000000000003BBF-constants end
-
-/**
- * Short description of class wfAuthoring_models_classes_ActivityService
+ * This file is part of TAO.
  *
  * @access public
  * @author Joel Bout, <joel.bout@tudor.lu>
