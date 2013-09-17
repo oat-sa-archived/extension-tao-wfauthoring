49512d3df5bb497d3eebb261a3be9f080412eb2e
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-17 14:49:50 +0000

    flatterner attempt 1
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/wfAuthoring/trunk@16247 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/class.ProcessFlattener.php
+++ models/classes/class.ProcessFlattener.php
@@ -1,5 +1,5 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -28,71 +28,89 @@
  * @package wfEngine
  * @subpackage models_classes
  */
-class wfAuthoring_models_classes_ProcessFlattener
-    extends wfAuthoring_models_classes_ProcessCloner{
-    
+class wfAuthoring_models_classes_ProcessFlattener extends wfAuthoring_models_classes_ProcessCloner
+{
+
     private $processDefinition;
-    
-    public function __construct(core_kernel_classes_Resource $processDefinition) {
+
+    public function __construct(core_kernel_classes_Resource $processDefinition){
         $this->processDefinition = $processDefinition;
         parent::__construct();
-    } 
-    
-    public function flatten() {
-        $activities = wfEngine_models_classes_ProcessDefinitionService::singleton()->getAllActivities($this->processDefinition);
-        foreach ($activities as $activity) {
-            $services = wfEngine_models_classes_ActivityService::singleton()->getInteractiveServices($activity);
-            // only replace single services
-            if (count($services) == 1) {
-                $serviceCall = current($services);
-                $serviceDefinition = $serviceCall->getUniquePropertyValue(new core_kernel_classes_Property(PROPERTY_CALLOFSERVICES_SERVICEDEFINITION));
-                if ($serviceDefinition->getUri() == INSTANCE_SERVICE_PROCESSRUNNER) {
-                    // found a wfEngine call, extract processDefnition
-                    
-                    $subProcess = $this->getSubProcess($serviceCall);
-                    if (empty($subProcess)) {
-                        throw new common_exception_InconsistentData('Missing process uri in service call '.$serviceCall->getUri());
-                    }
-                    
-                    // @todo test the process first
-                    
-                    // @todo clone sub process
-                    common_Logger::w('Should have cloned subprocess '.$subProcess);
-                    
-                    //$info = $this->cloneProcessSegment($subProcess);
-                    
-                    /*
-                    //clone the process segment:
-                    $testInterfaces = $this->processCloner->cloneProcessSegment($testProcess, false);
-                     
-                    if(!empty($testInterfaces['in']) && !empty($testInterfaces['out'])){
-                        $inActivity = $testInterfaces['in'];
-                        $firstout = current($testInterfaces['out']);
-                        common_Logger::i('Cloning T '.$activity->getUri().' to '.$inActivity->getUri().'=>'.$firstout->getUri());
-                        $this->processCloner->addClonedActivity($inActivity, $activity, $firstout);
-                    
-                        $toLink[] = $activity;
-                    }else{
-                        throw new Exception("the process segment of the test process {$testProcess->getUri()} cannot be cloned");
-                    }
-                    */
+    }
+
+    public function flatten(){
+
+        //clone activities and connectors
+        $processDefinitionService = wfEngine_models_classes_ProcessDefinitionService::singleton();
+        $activities = $processDefinitionService->getAllActivities($this->processDefinition);
+
+        foreach($activities as $activity){
+            $this->flattenProcessActivity($activity);
+        }
+
+    }
+
+    protected function flattenProcessActivity(core_kernel_classes_Resource $activity){
+        
+        $services = wfEngine_models_classes_ActivityService::singleton()->getInteractiveServices($activity);
+        // only replace single-service activities, with the service process runner
+        if(count($services) == 1){
+            $serviceCall = current($services);
+            $serviceDefinition = $serviceCall->getUniquePropertyValue(new core_kernel_classes_Property(PROPERTY_CALLOFSERVICES_SERVICEDEFINITION));
+            if($serviceDefinition->getUri() == INSTANCE_SERVICE_PROCESSRUNNER){
+                
+                // found a wfEngine call, extract processDefnition
+                $subProcess = $this->getSubProcess($serviceCall);
+                if(empty($subProcess)){
+                    throw new common_exception_InconsistentData('Missing process uri in service call '.$serviceCall->getUri());
+                }
+
+                // @todo test the process first
+                // @todo clone sub process
+                common_Logger::w('Should have cloned subprocess '.$subProcess);
+
+                $segment = $this->cloneProcessSegment($subProcess);
+                $inActivity = $segment['in'];
+                $firstout = current($segment['out']);
+                $this->addClonedActivity($inActivity, $activity, $firstout);
+
+                $propProcessActivities = new core_kernel_classes_Property(PROPERTY_PROCESS_ACTIVITIES);
+                foreach($this->getClonedActivities() as $activityClone){
+                    $this->processDefinition->setPropertyValue($propProcessActivities, $activityClone->getUri());
+                }
+
+                //get the previous connector if exists and clone it
+                $connectors = wfAuthoring_models_classes_ProcessService::singleton()->getConnectorsByActivity($activity);
+                $connectors = array_merge($connectors['next'], $connectors['prev']);
+                foreach($connectors as $connector){
+                    $this->addClonedConnector($connector, $connector);
+                }
+
+                //glue segment:
+                $this->linkClonedStep($activity);
+
+                //delete all activity:
+                $activity->delete(true);
+                
+                //recursive call:
+                foreach($this->getClonedActivities() as $activityClone){
+                    $this->flattenProcessActivity($activityClone);
                 }
             }
         }
     }
-    
-    protected function getSubProcess($service) {
+
+    protected function getSubProcess($service){
         $returnValue = null;
         $propertyIterator = $service->getPropertyValuesCollection(new core_kernel_classes_Property(PROPERTY_CALLOFSERVICES_ACTUALPARAMETERIN))->getIterator();
-        foreach ($propertyIterator as $actualParam) {
+        foreach($propertyIterator as $actualParam){
             $formalParam = $actualParam->getUniquePropertyValue(new core_kernel_classes_Property(PROPERTY_ACTUALPARAMETER_FORMALPARAMETER));
-            if ($formalParam->getUri() == INSTANCE_FORMALPARAM_PROCESSDEFINITION) {
+            if($formalParam->getUri() == INSTANCE_FORMALPARAM_PROCESSDEFINITION){
                 $returnValue = $actualParam->getOnePropertyValue(new core_kernel_classes_Property(PROPERTY_ACTUALPARAMETER_CONSTANTVALUE));
                 break;
             }
         }
         return $returnValue;
     }
-    
-    
+
 }
