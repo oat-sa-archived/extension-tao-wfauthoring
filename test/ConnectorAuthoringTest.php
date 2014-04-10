84469de532f7ca4f29c89914ee395cbb3d1f39bc
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-10 14:36:09 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/wfAuthoring/trunk@19608 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/ConnectorAuthoringTest.php
+++ test/ConnectorAuthoringTest.php
@@ -1,9 +1,7 @@
 <?php
 require_once dirname(__FILE__) . '/../../wfEngine/test/wfEngineServiceTest.php';
-/*
-require_once dirname(__FILE__) . '/../../tao/test/TaoTestRunner.php';
-include_once dirname(__FILE__) . '/../includes/raw_start.php';
-*/
+
+
 class ConnectorAuthoringTest extends wfEngineServiceTest {
 	
 	/**
